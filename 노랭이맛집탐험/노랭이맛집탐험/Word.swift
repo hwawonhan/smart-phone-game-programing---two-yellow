3bcc9fe88c9f893e4d2f3ea2a0af3011e9af233d
JiIn Min <mjiijm96@naver.com>
2018-05-27 00:36:53 +0900

    현재위치 정보 받아오기 완료

---
+++ 노랭이맛집탐험/노랭이맛집탐험/Word.swift
@@ -0,0 +1,18 @@
+//
+//  Word.swift
+//  노랭이맛집탐험
+//
+//  Created by kpugame on 2018. 5. 25..
+//  Copyright © 2018년 hwawonhan. All rights reserved.
+//
+import UIKit
+
+struct Restaurant {
+    var name: String?
+    var adress: String?
+    
+    init(name: String?, adress: String?) {
+        self.name = name
+        self.adress = adress
+    }
+}
---
+++ 노랭이맛집탐험/노랭이맛집탐험/WordViewController.swift
@@ -0,0 +1,166 @@
+//
+//  WordViewController.swift
+//  노랭이맛집탐험
+//
+//  Created by kpugame on 2018. 5. 25..
+//  Copyright © 2018년 hwawonhan. All rights reserved.
+//
+
+import UIKit
+
+class WordViewController: UITableViewController, XMLParserDelegate {
+    @IBOutlet var tbData: UITableView!
+    var parser = XMLParser()
+    var posts = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var title1 = NSMutableString()
+    var date = NSMutableString()
+    var foodRestaurants:[Restaurant] = foodData
+    
+    func beginParsing()
+    {
+        /*
+        String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
+        try {
+            String text = URLEncoder.encode("안녕하세요. 오늘 기분은 어떻습니까?", "UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+        */
+        var clientId: String = "1qO9XJIT5mR1O3sz_u6J"
+        var clientSecret: String = "bZcbo3uiSX"
+        var url: String = "https://openapi.naver.com/search/local"
+        
+        posts = []
+        parser = XMLParser(contentsOf:(URL(string:"https://openapi.naver.com/search/local/홍대맛집/10/1/random/ClientId=1qO9XJIT5mR1O3sz_u6J/ClientSecret=bZcbo3uiSX"))!)!
+        parser.delegate = self
+        parser.parse()
+        tbData!.reloadData()
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName:
+        String, namespaceURI: String?, qualifiedName qName: String?,
+                attributes attributeDict: [String : String])
+    {
+        element = elementName as NSString
+        if (elementName as NSString).isEqual(to: "item")
+        {
+            elements = NSMutableDictionary()
+            elements = [:]
+            title1 = NSMutableString()
+            title1 = ""
+            date = NSMutableString()
+            date = ""
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if element.isEqual(to: "name") {
+            title1.append(string)
+        } else if element.isEqual(to: "address") {
+            date.append(string)
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
+    {
+        if (elementName as NSString).isEqual(to: "item") {
+            if !title1.isEqual(nil) {
+                elements.setObject(title1, forKey: "name" as NSCopying)
+            }
+            if !date.isEqual(nil) {
+                elements.setObject(date, forKey: "address" as NSCopying)
+            }
+            posts.add(elements)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        beginParsing()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return posts.count
+    }
+
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = (posts.object(at: indexPath.row) as AnyObject).value(forKey: "title") as! NSString as String
+        cell.detailTextLabel?.text = (posts.object(at: indexPath.row) as AnyObject).value(forKey: "address") as! NSString as String
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
